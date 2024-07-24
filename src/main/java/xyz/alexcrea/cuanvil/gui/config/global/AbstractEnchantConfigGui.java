a6c2d5f1486b256ca9008013759e2dd73dc7a0a5
alexcrea <42614139+alexcrea@users.noreply.github.com>
2024-07-24 19:13:23 +0200

    Sort new enchantment in gui on add.

--- src/main/java/xyz/alexcrea/cuanvil/gui/config/global/AbstractEnchantConfigGui.java
+++ src/main/java/xyz/alexcrea/cuanvil/gui/config/global/AbstractEnchantConfigGui.java
@@ -53,6 +53,46 @@ public abstract class AbstractEnchantConfigGui<T extends SettingGui.SettingGuiFa
         );
     }
 
+    @Override
+    public void updateValueForGeneric(CAEnchantment generic, boolean shouldUpdate) {
+        updateValueForGeneric(generic, shouldUpdate, true);
+    }
+
+    public void updateValueForGeneric(CAEnchantment generic, boolean shouldUpdate, boolean prepareSorting) {
+        if(!prepareSorting) {
+            super.updateValueForGeneric(generic, shouldUpdate);
+            return;
+        }
+
+        if(!this.factoryMap.containsKey(generic)){
+            // We need to sort elements again
+            super.updateValueForGeneric(generic, false);
+
+            // Clear page then refill all of them
+            this.firstPage.clear();
+            this.pages.clear();
+            this.pages.add(this.firstPage);
+
+            for (CAEnchantment enchantment : getEveryDisplayableInstanceOfGeneric()) {
+                GuiItem item = this.guiItemMap.get(enchantment);
+
+                if(item == null) {
+                    updateValueForGeneric(enchantment, false, false);
+                }else {
+                    addToPage(item);
+                }
+
+            }
+
+            if(shouldUpdate) update();
+
+        }else{
+            super.updateValueForGeneric(generic, shouldUpdate);
+
+        }
+
+    }
+
 
     // Unused methods
     @Override
