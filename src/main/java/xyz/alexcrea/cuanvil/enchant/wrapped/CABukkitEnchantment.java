cf4cc740e89a6981f93cf374024f2a03970c8b85
alexcrea <42614139+alexcrea@users.noreply.github.com>
2024-08-28 14:58:43 +0200

    Better way to find rarity on paper.

--- src/main/java/xyz/alexcrea/cuanvil/enchant/wrapped/CABukkitEnchantment.java
+++ src/main/java/xyz/alexcrea/cuanvil/enchant/wrapped/CABukkitEnchantment.java
@@ -1,7 +1,9 @@
 package xyz.alexcrea.cuanvil.enchant.wrapped;
 
+import io.delilaheve.CustomAnvil;
 import io.delilaheve.util.ItemUtil;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -11,7 +13,12 @@ import xyz.alexcrea.cuanvil.enchant.CAEnchantmentBase;
 import xyz.alexcrea.cuanvil.enchant.EnchantmentProperties;
 import xyz.alexcrea.cuanvil.enchant.EnchantmentRarity;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+import java.util.logging.Level;
 
 /**
  * Custom Anvil enchantment implementation for vanilla registered enchantment.
@@ -94,7 +101,7 @@ public class CABukkitEnchantment extends CAEnchantmentBase {
         try {
             return EnchantmentProperties.valueOf(enchantment.getKey().getKey().toUpperCase(Locale.ENGLISH)).getRarity();
         } catch (IllegalArgumentException ignored) {
-            return EnchantmentRarity.COMMON;
+            return findRarity(enchantment);
         }
     }
 
@@ -102,4 +109,52 @@ public class CABukkitEnchantment extends CAEnchantmentBase {
     protected Enchantment getEnchant() {
         return this.enchantment;
     }
+
+    private static Method getAnvilCostMethod;
+    static {
+        Class<Enchantment> clazz = Enchantment.class;
+        try {
+            getAnvilCostMethod = clazz.getDeclaredMethod("getAnvilCost");
+            getAnvilCostMethod.setAccessible(true);
+
+            CustomAnvil.Companion.log("Detected getAnvilCost method");
+        } catch (NoSuchMethodException e) {
+            getAnvilCostMethod = null;
+        }
+
+    }
+
+    private static final Map<EnchantmentTarget, String> targetToGroup = new HashMap<>();
+    static {
+        targetToGroup.put(EnchantmentTarget.ARMOR, "armors");
+        targetToGroup.put(EnchantmentTarget.ARMOR_HEAD, "helmets");
+        targetToGroup.put(EnchantmentTarget.ARMOR_TORSO, "chestplate");
+        targetToGroup.put(EnchantmentTarget.ARMOR_LEGS, "leggings");
+        targetToGroup.put(EnchantmentTarget.ARMOR_FEET, "boots");
+        targetToGroup.put(EnchantmentTarget.BOW, "bow");
+        targetToGroup.put(EnchantmentTarget.BREAKABLE, "can_unbreak");
+        targetToGroup.put(EnchantmentTarget.CROSSBOW, "crossbow");
+        targetToGroup.put(EnchantmentTarget.FISHING_ROD, "fishing_rod");
+        targetToGroup.put(EnchantmentTarget.TOOL, "tools");
+        targetToGroup.put(EnchantmentTarget.TRIDENT, "trident");
+        targetToGroup.put(EnchantmentTarget.VANISHABLE, "can_vanish");
+        targetToGroup.put(EnchantmentTarget.WEAPON, "swords");
+        targetToGroup.put(EnchantmentTarget.WEARABLE, "wearable");
+    }
+
+    private static EnchantmentRarity findRarity(Enchantment enchantment) {
+        if(getAnvilCostMethod == null) return EnchantmentRarity.COMMON;
+
+        try {
+            int itemCost = (int) getAnvilCostMethod.invoke(enchantment);
+
+            return EnchantmentRarity.getRarity(itemCost);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            CustomAnvil.instance.getLogger().log(Level.SEVERE, "could not find cost for enchantment "+enchantment.getKey(), e);
+
+            return EnchantmentRarity.COMMON;
+        }
+
+    }
+
 }
