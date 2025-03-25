f5ba2cc1e262f59f52435bd24ba376729333cfb9
alexcrea <42614139+alexcrea@users.noreply.github.com>
2025-03-25 16:36:30 +0100

    prepare for 1.21.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,6 +29,8 @@ include("nms:v1_21R2")
 findProject(":nms:v1_21R2")?.name = "v1_21R2"
 include("nms:v1_21R3")
 findProject(":nms:v1_21R3")?.name = "v1_21R3"
+include("nms:v1_21R4")
+findProject(":nms:v1_21R4")?.name = "v1_21R4"
 
 include(":impl:LegacyEcoEnchant")
 findProject(":impl:LegacyEcoEnchant")?.name = "LegacyEcoEnchant"
