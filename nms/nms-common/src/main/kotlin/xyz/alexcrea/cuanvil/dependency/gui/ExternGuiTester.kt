bf7a158c9534162f436a29bb874997851b34f3fa
alexcrea <42614139+alexcrea@users.noreply.github.com>
2025-04-29 14:11:37 +0200

    fix incompability with xenondev anvil UI

--- nms/nms-common/src/main/kotlin/xyz/alexcrea/cuanvil/dependency/gui/ExternGuiTester.kt
+++ nms/nms-common/src/main/kotlin/xyz/alexcrea/cuanvil/dependency/gui/ExternGuiTester.kt
@@ -9,15 +9,29 @@ interface ExternGuiTester {
     fun getContainerClass(inventory: InventoryView): Class<Any>?
 
     fun testIfGui(inventory: InventoryView): Boolean {
-        val clazz = getContainerClass(inventory)
-        if(clazz == null) return false
-
-        val expectedWesjdGuiPath = "anvilgui.version.$wesjdAnvilGuiName"
+        val clazz = getContainerClass(inventory) ?: return false
 
         val clazzName = clazz.name
-        val isWejdsGui = clazzName.contains(expectedWesjdGuiPath)
+        if (expectWesjd(clazzName)) return true
+        if (expectXenondevUI(clazzName)) return true
 
-        return isWejdsGui
+        return false
+    }
+
+    fun expectWesjd(name: String): Boolean {
+        val expectedWesjdGuiPath = "anvilgui.version.$wesjdAnvilGuiName"
+
+        return name.contains(expectedWesjdGuiPath)
+    }
+
+    private val XenondevUIPrefix: String
+        get() = "xyz.xenondevs.inventoryaccess."
+    private val XenondevUISufix: String
+        get() = ".AnvilInventoryImpl"
+
+    fun expectXenondevUI(name: String): Boolean {
+        return name.startsWith(XenondevUIPrefix)
+                && name.endsWith(XenondevUISufix)
     }
 
 
