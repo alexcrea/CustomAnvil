78d548676d06da112bec4187609dc72ebda5b195
alexcrea <42614139+alexcrea@users.noreply.github.com>
2025-06-14 17:03:53 +0200

    remove legacy nms from code

--- src/main/kotlin/xyz/alexcrea/cuanvil/dependency/packet/PacketManagerSelector.kt
+++ src/main/kotlin/xyz/alexcrea/cuanvil/dependency/packet/PacketManagerSelector.kt
@@ -27,32 +27,6 @@ object PacketManagerSelector {
             return when (versionParts[1]) {
                 // Can't support 1.16.5 bc 1.16.5 paper userdev do not exist
 
-                17 -> when (versionParts[2]) {
-                    0, 1 -> V1_17R1_PacketManager()
-                    else -> null
-                }
-
-                18 -> when (versionParts[2]) {
-                    0, 1 -> V1_18R1_PacketManager()
-                    2 -> V1_18R2_PacketManager()
-                    else -> null
-                }
-
-                19 -> when (versionParts[2]) {
-                    0, 1, 2 -> V1_19R1_PacketManager()
-                    3 -> V1_19R2_PacketManager()
-                    4 -> V1_19R3_PacketManager()
-                    else -> null
-                }
-
-                20 -> when (versionParts[2]) {
-                    0, 1 -> V1_20R1_PacketManager()
-                    2 -> V1_20R2_PacketManager()
-                    3, 4 -> V1_20R3_PacketManager()
-                    5, 6 -> V1_20R4_PacketManager()
-                    else -> null
-                }
-
                 21 -> when (versionParts[2]) {
                     0, 1 -> V1_21R1_PacketManager()
                     2, 3 -> V1_21R2_PacketManager()
