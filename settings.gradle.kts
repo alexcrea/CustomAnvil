a8524fd8c821eb10e054e741638b90fd9523bcd0
alexcrea <42614139+alexcrea@users.noreply.github.com>
2025-06-14 16:58:12 +0200

    remove legacy plugin and server versions from gradlew

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,26 +3,6 @@ rootProject.name = "CustomAnvil"
 // NMS subproject
 include("nms:nms-common")
 findProject(":nms:nms-common")?.name = "nms-common"
-include("nms:v1_17R1")
-findProject(":nms:v1_17R1")?.name = "v1_17R1"
-include("nms:v1_18R1")
-findProject(":nms:v1_18R1")?.name = "v1_18R1"
-include("nms:v1_18R2")
-findProject(":nms:v1_18R2")?.name = "v1_18R2"
-include("nms:v1_19R1")
-findProject(":nms:v1_19R1")?.name = "v1_19R1"
-include("nms:v1_19R2")
-findProject(":nms:v1_19R2")?.name = "v1_19R2"
-include("nms:v1_19R3")
-findProject(":nms:v1_19R3")?.name = "v1_19R3"
-include("nms:v1_20R1")
-findProject(":nms:v1_20R1")?.name = "v1_20R1"
-include("nms:v1_20R2")
-findProject(":nms:v1_20R2")?.name = "v1_20R2"
-include("nms:v1_20R3")
-findProject(":nms:v1_20R3")?.name = "v1_20R3"
-include("nms:v1_20R4")
-findProject(":nms:v1_20R4")?.name = "v1_20R4"
 include("nms:v1_21R1")
 findProject(":nms:v1_21R1")?.name = "v1_21R1"
 include("nms:v1_21R2")
@@ -34,5 +14,3 @@ findProject(":nms:v1_21R4")?.name = "v1_21R4"
 include("nms:v1_21R5")
 findProject(":nms:v1_21R5")?.name = "v1_21R5"
 
-include(":impl:LegacyEcoEnchant")
-findProject(":impl:LegacyEcoEnchant")?.name = "LegacyEcoEnchant"
