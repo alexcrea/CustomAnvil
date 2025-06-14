f6a7878c1ea6a7976db58b5d153ead30276d29d0
alexcrea <42614139+alexcrea@users.noreply.github.com>
2025-06-14 17:03:53 +0200

    remove legacy nms from code

--- src/main/kotlin/xyz/alexcrea/cuanvil/dependency/gui/GuiTesterSelector.kt
+++ src/main/kotlin/xyz/alexcrea/cuanvil/dependency/gui/GuiTesterSelector.kt
@@ -1,6 +1,6 @@
 package xyz.alexcrea.cuanvil.dependency.gui
 
-import xyz.alexcrea.cuanvil.dependency.gui.version.*;
+import xyz.alexcrea.cuanvil.dependency.gui.version.*
 import xyz.alexcrea.cuanvil.update.UpdateUtils
 
 object GuiTesterSelector {
@@ -12,32 +12,6 @@ object GuiTesterSelector {
 
             return when (versionParts[1]) {
                 // Can't support 1.16.5 bc 1.16.5 paper userdev do not exist
-
-                17 -> when (versionParts[2]) {
-                    0, 1 -> v1_17R1_ExternGuiTester()
-                    else -> null
-                }
-
-                18 -> when (versionParts[2]) {
-                    0, 1 -> v1_18R1_ExternGuiTester()
-                    2 -> v1_18R2_ExternGuiTester()
-                    else -> null
-                }
-
-                19 -> when (versionParts[2]) {
-                    0, 1, 2 -> v1_19R1_ExternGuiTester()
-                    3 -> v1_19R2_ExternGuiTester()
-                    4 -> v1_19R3_ExternGuiTester()
-                    else -> null
-                }
-
-                20 -> when (versionParts[2]) {
-                    0, 1 -> v1_20R1_ExternGuiTester()
-                    2 -> v1_20R2_ExternGuiTester()
-                    3, 4 -> v1_20R3_ExternGuiTester()
-                    5, 6 -> v1_20R4_ExternGuiTester()
-                    else -> null
-                }
 
                 21 -> when (versionParts[2]) {
                     0, 1 -> v1_21R1_ExternGuiTester()
