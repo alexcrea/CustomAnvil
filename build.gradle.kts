2a36ad945d1412b8a9d91fe647dc9705f143a975
alexcrea <42614139+alexcrea@users.noreply.github.com>
2025-06-23 22:04:20 +0200

    fix me being stupid

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 }
 
 group = "xyz.alexcrea"
-version = "1.12.0"
+version = "1.12.1"
 
 val effectiveVersion = "$version" +
         (if (System.getenv("SMALL_COMMIT_HASH") != null) "-dev-${System.getenv("SMALL_COMMIT_HASH")!!}" else "")
