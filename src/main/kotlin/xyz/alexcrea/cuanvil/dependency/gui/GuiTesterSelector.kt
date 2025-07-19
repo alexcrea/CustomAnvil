3d58bb256520b039c781b10bc4bb4ccef4080c8d
alexcrea <contact@alexcrea.xyz>
2025-07-19 21:05:59 +0200

    1.21.8

--- src/main/kotlin/xyz/alexcrea/cuanvil/dependency/gui/GuiTesterSelector.kt
+++ src/main/kotlin/xyz/alexcrea/cuanvil/dependency/gui/GuiTesterSelector.kt
@@ -44,7 +44,7 @@ object GuiTesterSelector {
                     2, 3 -> v1_21R2_ExternGuiTester()
                     4 -> v1_21R3_ExternGuiTester()
                     5 -> v1_21R4_ExternGuiTester()
-                    6, 7 -> v1_21R5_ExternGuiTester()
+                    6, 7, 8 -> v1_21R5_ExternGuiTester()
                     else -> null
                 }
 
