be2d284a6f4475f734487674f291826c175c313d
alexcrea <contact@alexcrea.xyz>
2025-07-27 06:59:01 +0200

    plugin version as beta

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 }
 
 group = "xyz.alexcrea"
-version = "1.14.1"
+version = "2.0.0-beta"
 
 val effectiveVersion = "$version" +
         (if (System.getenv("SMALL_COMMIT_HASH") != null) "-dev-${System.getenv("SMALL_COMMIT_HASH")!!}" else "")
