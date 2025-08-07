54801a07e6cc4d767b59ae2293608508ba089418
alexcrea <contact@alexcrea.xyz>
2025-08-07 10:40:30 +0200

    update protocolib to use maven central

--- nms/nms-common/build.gradle.kts
+++ nms/nms-common/build.gradle.kts
@@ -1,16 +1,10 @@
 group = rootProject.group
 version = rootProject.version
 
-repositories {
-    // ProtocoLib
-    maven (url = "https://repo.dmulloy2.net/repository/public/" )
-
-}
-
 dependencies {
     // Spigot api
     compileOnly("org.spigotmc:spigot-api:1.18-R0.1-SNAPSHOT")
 
     // Protocolib
-    compileOnly("com.comphenix.protocol:ProtocolLib:5.1.0")
+    compileOnly("net.dmulloy2:ProtocolLib:5.4.0")
 }
