8afd54c94de042cd3efd9f4e4e0854f0a25974a2
alexcrea <contact@alexcrea.xyz>
2025-11-07 00:02:17 +0100

    Custom anvil only work on nms menu

--- nms/nms-common/src/main/kotlin/xyz/alexcrea/cuanvil/dependency/gui/ExternGuiTester.kt
+++ nms/nms-common/src/main/kotlin/xyz/alexcrea/cuanvil/dependency/gui/ExternGuiTester.kt
@@ -1,8 +1,13 @@
 package xyz.alexcrea.cuanvil.dependency.gui
 
 import org.bukkit.inventory.InventoryView
+import xyz.alexcrea.cuanvil.dependency.util.PlatformUtil
 
 interface ExternGuiTester {
+
+    object Const{
+        val cannonicalPaperAnvilMenu = "net.minecraft.world.inventory.AnvilMenu"
+    }
 
     val wesjdAnvilGuiName: String?
 
@@ -16,12 +21,17 @@ interface ExternGuiTester {
         val clazz = getContainerClass(inventory) ?: return false
 
         val clazzName = clazz.name
-        //TODO maybe instead of testing non default, better to be testing we are default ?
-        if (expectWesjd(clazzName)) return true
-        if (expectXenondevUI(clazzName)) return true
-        if (expectVanePortal(clazzName)) return true
+        if(!PlatformUtil.isPaper){
+            // Blacklist gui causing issue
+            if (expectWesjd(clazzName)) return true
+            if (expectXenondevUI(clazzName)) return true
+            if (expectVanePortal(clazzName)) return true
 
-        return false
+            return false
+        }
+
+        // Only allow cannonical anvil menu class
+        return !Const.cannonicalPaperAnvilMenu.equals(clazzName, true)
     }
 
     fun expectWesjd(name: String): Boolean {
