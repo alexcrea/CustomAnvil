8582038c715c1db187b2728d4deff6877bd178cc
alexcrea <contact@alexcrea.xyz>
2025-12-01 19:19:50 +0100

    add unit repair for spears

--- src/main/java/xyz/alexcrea/cuanvil/update/minecraft/Update_1_21_11.java
+++ src/main/java/xyz/alexcrea/cuanvil/update/minecraft/Update_1_21_11.java
@@ -38,7 +38,26 @@ public class Update_1_21_11 extends MCUpdate{
         addAbsentToList(conflictConfig, "restriction_fire_aspect.notAffectedGroups", "spears");
 
         // Unit repair for spears
+        unitConfig.set("gold_ingot.golden_spear", 0.25);
+        unitConfig.set("copper_ingot.copper_spear", 0.25);
+        unitConfig.set("iron_ingot.iron_spear", 0.25);
+        unitConfig.set("diamond.diamond_spear", 0.25);
+        unitConfig.set("netherite_ingot.netherite_spear", 0.25);
 
+        unitConfig.set("cobblestone.stone_spear", 0.25);
+        unitConfig.set("cobbled_deepslate.stone_spear", 0.25);
+
+        unitConfig.set("oak_planks.wooden_spear", 0.25);
+        unitConfig.set("spruce_planks.wooden_spear", 0.25);
+        unitConfig.set("birch_planks.wooden_spear", 0.25);
+        unitConfig.set("jungle_planks.wooden_spear", 0.25);
+        unitConfig.set("acacia_planks.wooden_spear", 0.25);
+        unitConfig.set("dark_oak_planks.wooden_spear", 0.25);
+        unitConfig.set("mangrove_planks.wooden_spear", 0.25);
+        unitConfig.set("cherry_planks.wooden_spear", 0.25);
+        unitConfig.set("bamboo_planks.wooden_spear", 0.25);
+        unitConfig.set("crimson_planks.wooden_spear", 0.25);
+        unitConfig.set("warped_planks.wooden_spear", 0.25);
 
         // Create lunge enchant value and group
         baseConfig.set("enchant_limits.minecraft:lunge", 3);
