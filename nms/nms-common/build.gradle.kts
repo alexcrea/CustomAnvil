1544cd315b7f15759f40460221a06f744d494020
alexcrea <contact@alexcrea.xyz>
2025-12-26 00:51:14 +0100

    fix older version issue

--- nms/nms-common/build.gradle.kts
+++ nms/nms-common/build.gradle.kts
@@ -21,8 +21,8 @@ repositories {
 
 // Set target version
 tasks.withType<JavaCompile>().configureEach {
-    sourceCompatibility = "21"
-    targetCompatibility = "21"
+    sourceCompatibility = "16"
+    targetCompatibility = "16"
 
     options.encoding = "UTF-8"
 }
@@ -30,6 +30,6 @@ tasks.withType<JavaCompile>().configureEach {
 kotlin {
     compilerOptions {
         apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
-        jvmTarget.set(JvmTarget.JVM_21)
+        jvmTarget.set(JvmTarget.JVM_16)
     }
 }
