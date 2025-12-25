fc94dbe16980f08f690128f5ee97ddcaf0465bb1
alexcrea <contact@alexcrea.xyz>
2025-12-25 22:33:37 +0100

    Generic gui tester and generic paper nms

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,6 +73,7 @@ dependencies {
 
     // Include nms
     implementation(project(":nms:nms-common"))
+    implementation(project(":nms:nms-paper"))
     implementation(project(":nms:v1_17R1", configuration = "reobf"))
     implementation(project(":nms:v1_18R1", configuration = "reobf"))
     implementation(project(":nms:v1_18R2", configuration = "reobf"))
