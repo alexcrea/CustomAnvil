474ad0f1b2245d7b575160a454c0dcd95ca8ab89
alexcrea <contact@alexcrea.xyz>
2026-01-01 19:01:56 +0100

    version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 }
 
 group = "xyz.alexcrea"
-version = "1.15.8"
+version = "1.15.9"
 
 val effectiveVersion = "$version" +
         (if (System.getenv("SMALL_COMMIT_HASH") != null) "-dev-${System.getenv("SMALL_COMMIT_HASH")!!}" else "")
