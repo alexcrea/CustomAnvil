3d50e0ec8291fc21bc5584d00a1d58aac49ad330
alexcrea <contact@alexcrea.xyz>
2026-03-24 01:40:46 +0100

    bruh

--- src/main/java/xyz/alexcrea/cuanvil/gui/config/settings/IntSettingsGui.java
+++ src/main/java/xyz/alexcrea/cuanvil/gui/config/settings/IntSettingsGui.java
@@ -92,7 +92,7 @@ public class IntSettingsGui extends AbstractSettingGui {
         // minus item
         GuiItem minusItem;
         if (now > holder.min) {
-            int planned = Math.min(holder.max, now + step);
+            int planned = Math.max(holder.min, now - step);
             minusItem = valueEditItem(Material.RED_TERRACOTTA, ValueDisplayType.REMOVE, planned);
         } else {
             minusItem = GuiGlobalItems.backgroundItem(Material.BARRIER);
@@ -102,7 +102,7 @@ public class IntSettingsGui extends AbstractSettingGui {
         //plus item
         GuiItem plusItem;
         if (now < holder.max) {
-            int planned = Math.max(holder.min, now - step);
+            int planned = Math.min(holder.max, now + step);
             plusItem = valueEditItem(Material.GREEN_TERRACOTTA, ValueDisplayType.ADD, planned);
         } else {
             plusItem = GuiGlobalItems.backgroundItem(Material.BARRIER);
