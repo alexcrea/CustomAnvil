5f0a601e5b8d925e305d58eee9798861b169ccc2
alexcrea <contact@alexcrea.xyz>
2026-06-28 23:01:56 +0200

    add edit config tab completer

--- src/main/kotlin/xyz/alexcrea/cuanvil/command/EditConfigExecutor.kt
+++ src/main/kotlin/xyz/alexcrea/cuanvil/command/EditConfigExecutor.kt
@@ -1,10 +1,12 @@
 package xyz.alexcrea.cuanvil.command
 
+import com.willfp.eco.core.lookup.matches
 import io.delilaheve.CustomAnvil
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.HumanEntity
 import xyz.alexcrea.cuanvil.dependency.util.PlatformUtil
+import xyz.alexcrea.cuanvil.enchant.CAEnchantmentRegistry
 import xyz.alexcrea.cuanvil.gui.config.MainConfigGui
 import xyz.alexcrea.cuanvil.gui.util.GuiGlobalActions
 
@@ -44,6 +46,30 @@ class EditConfigExecutor: CASubCommand() {
 
     override fun description(): String {
         return "Gui to edit the plugin's config"
+    }
+
+    private fun allEnchantmentsByName(): Collection<String>{
+        val names = mutableSetOf<String>()
+        for (enchantment in CAEnchantmentRegistry.getInstance().values()) {
+            names.add(enchantment.name)
+            names.add(enchantment.key.toString())
+        }
+
+        return names
+    }
+
+    override fun tabCompleter(sender: CommandSender, args: Array<out String>, list: MutableList<String>) {
+        list.addAll(when (args.size) {
+            1 -> listOf("item", "enchant", "open")
+            2 -> {
+                when(args[0].lowercase()) {
+                    "enchant" -> allEnchantmentsByName()
+                    else -> listOf()
+                }
+            }
+            else -> listOf()
+        })
+
     }
 
 }
