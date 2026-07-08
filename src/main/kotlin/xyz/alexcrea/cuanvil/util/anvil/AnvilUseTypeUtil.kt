cf768d67bd57bdaaadc4657ddae5c2dbaf46e89b
alexcrea <contact@alexcrea.xyz>
2026-07-08 03:42:14 +0200

    more proper comment [ci skip]

--- src/main/kotlin/xyz/alexcrea/cuanvil/util/anvil/AnvilUseTypeUtil.kt
+++ src/main/kotlin/xyz/alexcrea/cuanvil/util/anvil/AnvilUseTypeUtil.kt
@@ -5,16 +5,11 @@ import io.delilaheve.util.ConfigOptions
 object AnvilUseTypeUtil {
 
     /*
-     By stupidity of kotlin not allowing static function outside of companion object I need to do another util class only for 1 function:
+     By kotlin not having static function outside of companion object I need to do another util class only for 1 function:
      Companion object on enum class seems to get initialized AFTER the enum itself
-     that mean. if you want to call a static function on the enum class itself YOU CAN'T. you need to do this stupid thing
-     or you can make a stupid top level function OR object that even worse because of global scope pollution
-     (btw was gpt ""solution"". fortunately I do not "vibe code" so I do what I know instead of stupid AI solution & code)
-     I mean, this is still global scope pollution bc of a USELESS class that SHOULD not exist but as a class is better than a random *ss function
-
-     sorry for the rent but this made me frustrated
-
-     Note: I still like a lot of part of kotlin compared to java but this part is one that I hate
+     that mean. if you want to call a static function on enum construction YOU CAN'T. you need to do this stupid thing,
+     or you can make a stupid top level function or object that at least as bad as this
+     I mean, this is still an unnecessary class that should not exist but as a class is better than a random function in my opinion
      */
     /**
      * Get config path for normal anvil use
