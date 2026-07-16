7b232528f1e0deb37e4d7a97be99516d1905ecaa
alexcrea <contact@alexcrea.xyz>
2026-07-16 18:44:23 +0200

    fix rename text not acting as vanilla

--- src/main/kotlin/xyz/alexcrea/cuanvil/anvil/AnvilMergeLogic.kt
+++ src/main/kotlin/xyz/alexcrea/cuanvil/anvil/AnvilMergeLogic.kt
@@ -6,6 +6,7 @@ import io.delilaheve.util.EnchantmentUtil.combineWith
 import io.delilaheve.util.ItemUtil.isEnchantedBook
 import io.delilaheve.util.ItemUtil.repairFrom
 import io.delilaheve.util.ItemUtil.unitRepair
+import net.kyori.adventure.text.Component
 import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.entity.HumanEntity
@@ -17,6 +18,7 @@ import org.bukkit.inventory.meta.ItemMeta
 import org.bukkit.persistence.PersistentDataType
 import xyz.alexcrea.cuanvil.api.EnchantmentApi
 import xyz.alexcrea.cuanvil.dependency.DependencyManager
+import xyz.alexcrea.cuanvil.dependency.util.PlatformUtil.setComponentDisplayName
 import xyz.alexcrea.cuanvil.dialog.AnvilRenameDialog
 import xyz.alexcrea.cuanvil.enchant.CAEnchantment
 import xyz.alexcrea.cuanvil.recipe.AnvilCustomRecipe
@@ -139,11 +141,12 @@ object AnvilMergeLogic {
     private fun handleRename(resultItem: ItemStack, inventory: AnvilInventory, player: HumanEntity): Int {
         // Can be null
         var renameText = ChatColor.stripColor(inventory.renameText)
+        var component: Component? = null
 
         var sumCost = 0
         var useColor = false
         if (ConfigOptions.renameColorPossible && renameText != null) {
-            val component = AnvilColorUtil.handleColor(
+            component = AnvilColorUtil.handleColor(
                 renameText,
                 AnvilColorUtil.renamePermission(player)
             )
@@ -170,7 +173,11 @@ object AnvilMergeLogic {
                         renameText == CasedStringUtil.snakeToUpperSpacedCase(resultItem.type.name.lowercase())
                         )
             ) {
-                it.setDisplayName(renameText)
+                if (component == null)
+                    component = if (renameText == null || renameText.isEmpty()) null
+                    else Component.text(renameText)
+                it.setComponentDisplayName(component, renameText)
+
                 processDialogPCD(it, player)
                 resultItem.itemMeta = it
 
