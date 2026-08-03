d6af0e8b8f76190dbc3628be5c955059ea5ee627
alexcrea <contact@alexcrea.xyz>
2026-08-03 14:10:23 +0200

    fix some custom recipe not working

--- src/main/kotlin/xyz/alexcrea/cuanvil/listener/AnvilResultListener.kt
+++ src/main/kotlin/xyz/alexcrea/cuanvil/listener/AnvilResultListener.kt
@@ -216,10 +216,10 @@ class AnvilResultListener : Listener {
         val recipe = result.recipe!!
 
         // We remove what should be removed
-        if (rightItem != null) {
-            if (recipe.rightItem == null) return false// in case it changed
+        if (!rightItem.isAir) {
+            if (recipe.rightItem.isAir) return false// in case it changed
 
-            rightItem.amount -= amount * recipe.rightItem!!.amount
+            rightItem!!.amount -= amount * recipe.rightItem!!.amount
             inventory.setItem(ANVIL_INPUT_RIGHT, rightItem)
         }
 
