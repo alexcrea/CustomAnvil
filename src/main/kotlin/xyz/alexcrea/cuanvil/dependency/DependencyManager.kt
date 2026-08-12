ab47a9df93da835a56b90c6002e74575a1534812
alexcrea <contact@alexcrea.xyz>
2026-08-12 07:24:21 +0200

    proper eco item check

--- src/main/kotlin/xyz/alexcrea/cuanvil/dependency/DependencyManager.kt
+++ src/main/kotlin/xyz/alexcrea/cuanvil/dependency/DependencyManager.kt
@@ -42,6 +42,7 @@ object DependencyManager {
 
     var enchantmentSquaredCompatibility: EnchantmentSquaredDependency? = null
     var ecoEnchantCompatibility: EcoEnchantDependency? = null
+    var hasEcoItem: Boolean = false
     var excellentEnchantsCompatibility: ExcellentEnchantsDependency? = null
 
     var disenchantmentCompatibility: DisenchantmentDependency? = null
@@ -77,6 +78,10 @@ object DependencyManager {
         if (pluginManager.isPluginEnabled("EcoEnchants")) {
             ecoEnchantCompatibility = EcoEnchantDependency(pluginManager.getPlugin("EcoEnchants")!!)
             ecoEnchantCompatibility!!.disableAnvilListener()
+        }
+        // EcoItem check
+        if (pluginManager.isPluginEnabled("EcoItems")) {
+            hasEcoItem = true
         }
 
         // Excellent Enchants dependency
