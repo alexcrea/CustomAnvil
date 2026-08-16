ca89b0d095dfc2250ab7044e3b1a04c33e32eeea
alexcrea <contact@alexcrea.xyz>
2026-08-16 08:44:58 +0200

    go back to old datapack manager

--- nms/nms-paper/src/main/kotlin/xyz/alexcrea/cuanvil/dependency/datapack/DataPackTester.kt
+++ nms/nms-paper/src/main/kotlin/xyz/alexcrea/cuanvil/dependency/datapack/DataPackTester.kt
@@ -1,20 +1,40 @@
+@file:Suppress("DEPRECATION", "removal")
+
 package xyz.alexcrea.cuanvil.dependency.datapack
 
 import io.papermc.paper.datapack.Datapack
 import org.bukkit.Bukkit
+import org.bukkit.packs.DataPack
 import java.util.*
 
 object DataPackTester {
+    val legacyNames: List<String>
+        get() = Bukkit.getDataPackManager().dataPacks
+            .stream().filter { obj -> obj.isEnabled }
+            .map { pack -> pack.key.key }
+            .toList()
 
     val enabledPacks: List<String>
         get() {
-            return try {
-                Bukkit.getDatapackManager().enabledPacks
+            try {
+                // will throw error if do not exist
+                Bukkit::class.java.getDeclaredMethod("getDatapackManager")
+
+                return Bukkit.getDatapackManager().enabledPacks
                     .stream().map { obj: Datapack -> obj.name }
                     .toList()
-            } catch (_: Exception){
+            } catch(_: NoSuchMethodException) {
+                try {
+                    DataPack::class.java.getDeclaredMethod("getKey")
+                } catch(e: NoSuchMethodException) {
+                    System.err.println("Could not find compatible datapack manager")
+                    System.err.println("If you are using a datapack that should be compatible with CustomAnvil. It will not get detected...")
+                    return emptyList()
+                }
+                return legacyNames
+            } catch(_: Exception) {
                 // Assume cause UnimplementedOperationException on mock server
-                Collections.emptyList()
+                return Collections.emptyList()
             }
         }
 }
