cd19e78a5274ca18c316e978f3e594be95bb2dc5
alexcrea <contact@alexcrea.xyz>
2026-09-09 19:22:00 +0200

    update faststats

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     compileOnly("org.spigotmc:spigot-api:1.21-R0.1-SNAPSHOT")
 
     // fast stats
-    implementation("dev.faststats.metrics:bukkit:0.29.0")
+    implementation("dev.faststats.metrics:bukkit:0.30.1")
 
     // minimessage
     implementation("net.kyori:adventure-text-minimessage:4.25.0")
