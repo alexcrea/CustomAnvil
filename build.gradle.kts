5b6bb9520e5a5189bbebccb7d6f19cab5658ca05
alexcrea <contact@alexcrea.xyz>
2026-09-21 21:07:45 +0200

    version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 }
 
 group = "xyz.alexcrea"
-version = "2.1.3"
+version = "2.1.4"
 
 val isDevBuild = System.getenv("SMALL_COMMIT_HASH") != null
 val isPreRelease = System.getenv("IS_GITHUB_PRERELEASE") == "true"
